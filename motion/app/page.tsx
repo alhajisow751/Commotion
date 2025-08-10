12546c8ab9f292e3a1051911a58a3e53d5f80642
Alhaji <abubakarr751@gmail.com>
2025-08-10 21:38:59 +0300

    Feat: Shadcn button components added

--- motion/app/page.tsx
+++ motion/app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <p className='text-red-500 font-semibold'> Hello Motion </p>
+    <>
+   <div>
+
+    <Button >
+      Delete
+    </Button>
+   
+   </div>
+   </>
    
   );
 }
